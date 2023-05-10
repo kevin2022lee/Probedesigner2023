c4b24933e9dd2fa52806cc9fc823a45a775b2f9e
lxk@yhkodia.com <lxk@yhkodia.com@4d950dd5-4564-4c36-a370-d57b197188d4>
2023-05-10 09:01:09 +0000

    中文乱码

--- 1/OligoDesigner/xmerCalcNSH.py
+++ 1/OligoDesigner/xmerCalcNSH.py
@@ -214,7 +214,7 @@ class CalcNSH:
         uni_CE_TAIL_seq=Seq("CTCTTGGAAAGAAAGTGAAGTG", IUPAC.unambiguous_dna)
         uni_LE_TAIL_seq=Seq("JTATJCGCJCTGFTATJCCGT", IUPAC.unambiguous_dna)
         
-        ########LCS############# 3.0系统的NSH主要来源于Linkers！2023.5.10
+        ########LCS############# 3.0 NSH all from Linkers！2023.5.10
         Calc_Seq=Seq(Calc_Seq, IUPAC.unambiguous_dna).upper()
         
         x4merlcs_AB=self.lcs(str(uni_AB_seq),str(Calc_Seq))
